2bbf419ec82a6b73eddc2ae031d03e568daec01b
Emydev-op <alphaoseghe@gmail.com>
2023-09-24 20:12:51 +0100

    created a relationship between the two models

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import BlogsModel
-
+from .models import BlogsModel, Comment
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        
 class BlogSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = BlogsModel
         fields = '__all__'
