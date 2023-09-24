a6f7f61b07800d163f505073b9f242b9c489cb4c
Emydev-op <alphaoseghe@gmail.com>
2023-09-24 22:09:13 +0100

    created a relationship between the two models

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path('blogs/', views.BlogListView.as_view(), name='blog-list'),
     path('blogs/<int:pk>/', views.BlogDetailsView.as_view(), name='blog-details'),
+    # path('comments/', views.CommentListView.as_view(), name='comment-list'),
+    # path('comments/<int:pk>/', views.CommentDetailView.as_view(), name='comment-detail'),
+    
+    path('blogs/<int:pk>/comments/', views.CommentListView.as_view(), name='comment-list'),
+    path('blogs/comments/<int:pk>/', views.CommentDetailView.as_view(), name='comment-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import BlogsModel
-from .serializers import BlogSerializer
+from .models import BlogsModel, Comment
+from .serializers import BlogSerializer, CommentSerializer
 # Create your views here.
 
 class BlogListView(generics.ListCreateAPIView):
@@ -11,3 +11,15 @@ class BlogListView(generics.ListCreateAPIView):
 class BlogDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogsModel.objects.all()
     serializer_class = BlogSerializer
+
+class CommentListView(generics.ListCreateAPIView):
+    # queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        Comment.objects.filter(post=pk)
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
