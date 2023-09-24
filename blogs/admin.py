723a203899d677dea3b69a70461a69b519342389
Emydev-op <alphaoseghe@gmail.com>
2023-09-24 19:47:10 +0100

    Created the comment models

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BlogsModel
+from .models import BlogsModel, Comment
 # Register your models here.
 
 admin.site.register(BlogsModel)
+admin.site.register(Comment)
--- blogs/models.py
+++ blogs/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogsModel(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=2000, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
      
     class Meta:
         ordering = ["title"]
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(BlogsModel, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text + " " + self.author.username
